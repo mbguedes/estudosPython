a9028302ea51fb60ee656109371ac1557201d81b
mbguedes <marcio.bodiniguedes@gmail.com>
2025-02-07 02:00:01 -0300

    mais alguns estudos basicos de python

---
+++ parOuImpar.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número e falarei se ele é par ou ímpar: "))
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
+
+# numero = int(input("Digite um número: "))
+# print("O número é par." if numero % 2 == 0 else "O número é ímpar.")
+# poderia fazer assim também, mas acho que a primeira forma é mais legível.
